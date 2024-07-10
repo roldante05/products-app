a8a38467dd8e45b02f37ff2a9cc94ed7197b47c2
Dante Roldan <droldan@socialnet.com.ar>
2024-07-10 14:26:44 -0300

    fix bug session

--- app/Http/Middleware/CheckUserSession.php
+++ app/Http/Middleware/CheckUserSession.php
@@ -3,16 +3,19 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
 class CheckUserSession
 {
     public function handle($request, Closure $next)
     {
-        if (!Session::has('username')) {
+        if (!Session::has('user')) {
             return redirect('/login');
         }
-
+    
+        Log::info('User session exists:', Session::get('user'));
         return $next($request);
     }
+    
 }
